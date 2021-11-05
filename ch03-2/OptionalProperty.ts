5c0fcd6bcd0a6535099814bd09ec57570d482b98
원치현 <heungeob1003@gmail.com>
2021-11-05 15:42:55 +0900

    인터페이스의 속성 구문 실습

---
+++ ch03-2/OptionalProperty.ts
@@ -0,0 +1,16 @@
+interface IPerson2 {
+    name: string // 필수 속성
+    age: number // 필수 속성
+    etc?: boolean // 선택 속성
+}
+
+let good1: IPerson2 = {
+    name:'Jack',
+    age: 32
+}
+
+let good2: IPerson2 = {
+    name:'Jack',
+    age: 32,
+    etc: true
+}
