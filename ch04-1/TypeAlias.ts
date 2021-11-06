d1ec1efeb41c75399023e9558f9111977decf95e
원치현 <heungeob1003@gmail.com>
2021-11-06 14:45:42 +0900

    타입스크립트 일등 함수 first-class function 실습

--- ch04-1/TypeAlias.ts
+++ ch04-1/TypeAlias.ts
@@ -1,3 +1,3 @@
-type stringNumberFunc = (string, number) => void
+type stringNumberFunc = (string, number) => void // 매개변수 2개와 함수반환값 타입을 새로운 stringNumberFunc이라는 타입으로 바꿔서 사용하게 해주느 type 키워드
 let f: stringNumberFunc = function(a: string, b: number): void {}
 let g: stringNumberFunc = function(c: string, d: number): void {}
---
+++ ch04-2/first-classFunction.ts
@@ -0,0 +1,4 @@
+let f = function(a, b) {return a + b}
+f = function(a, b) {return a - b} 
+
+//일등 함수 = 함수와 변수를 차별(구분)하지 않는다.
