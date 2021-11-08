052eaaeb58743800481c0380baeae7e267533995
원치현 <heungeob1003@gmail.com>
2021-11-08 20:30:05 +0900

    filter 메서드 실습

---
+++ ch05-2/filter.ts
@@ -0,0 +1,8 @@
+const range = (from: number, to: number): number[] =>
+    from < to ? [from, ...range(from + 1, to)] : []
+
+const array: number[] = range(1, 10 + 1)
+
+let odds: number[] = array.filter((value) => value % 2 != 0)
+let evens: number[] = array.filter((value) => value % 2 = 0)
+console.log(odds, evens) // [1, 3, 5, 7, 9] [2, 4, 6, 8, 10]
